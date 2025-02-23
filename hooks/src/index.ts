4adb20616204bcb01b32d7ea09f63482c0fa0a00
prasadbylapudi <prasadbylapudi123@gmail.com>
2025-02-23 19:45:49 +0530

    zapruns created

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,19 +1,40 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const client=new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
 //https:hooks.zapier.com/hooks/catch/123456/abcde
 //password logic, only few people can access this endpoint
-app.post('/hooks/catch/:userId/:zapId', (req, res) => {
+app.post('/hooks/catch/:userId/:zapId', async(req, res) => {
 
     const userId= req.params.userId;
     const zapId= req.params.zapId;
-
+    const body=req.body;
     //store in db a new trigger.
 
-    //push it to a queue( redis/ kafka)
-    console.log(req.body);
-    res.send('OK');   
+    try {
+        await client.$transaction(async (tx) => {
+            const run = await client.zapRun.create({
+                data:{
+                    zapId:zapId,
+                    metadata:body,   
+                }
+            });
+        });
+
+        //push it to a queue( redis/ kafka)
+        console.log(req.body);
+        res.send('OK');
+    } catch (error) {
+        console.error('Error creating zapRun:', error);
+        res.status(500).send('Internal Server Error');
+    }
 })
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
